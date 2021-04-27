56d1c665f04c368a9f466eadefb6e4767bf75912
Lancelot Gordon <lance032017@gmail.com>
2021-04-27 17:14:15 -0400

    update FA forms

--- src/assets/fa_validation.js
+++ src/assets/fa_validation.js
@@ -22,33 +22,3 @@
   });
 
 })(jQuery);
-
-(function($) {
-
-  function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++) {
-      hash = hashes[i].split('=');
-      vars.push(hash[0].toLowerCase());
-      vars[hash[0].toLowerCase()] = hash[1];
-    }
-    return vars;
-  }
-  var queryStringArr = getUrlVars();
-
-  $('#tfa_107').val(queryStringArr['utm_medium'] || '');
-  $('#tfa_109').val(queryStringArr['utm_source'] || '');
-  $('#tfa_111').val(queryStringArr['utm_campaign'] || '');
-  $('#tfa_116').val(queryStringArr['utm_term'] || '');
-  $('#tfa_117').val(queryStringArr['utm_content'] || '');
-  $('#tfa_118').val(window.location.href);
-
-  $('#tfa_96').val(queryStringArr['utm_medium'] || '');
-  $('#tfa_97').val(queryStringArr['utm_source'] || '');
-  $('#tfa_98').val(queryStringArr['utm_campaign'] || '');
-  $('#tfa_99').val(queryStringArr['utm_term'] || '');
-  $('#tfa_100').val(queryStringArr['utm_content'] || '');
-  $('#tfa_101').val(window.location.href);
-
-})(jQuery);
