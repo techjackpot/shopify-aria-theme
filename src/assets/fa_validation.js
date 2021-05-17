1d0c73a708043dd57c6bab214cc14960fe671620
Lancelot Gordon <lance032017@gmail.com>
2021-05-17 11:16:40 -0400

    formassembly form part2 validation

--- src/assets/fa_validation.js
+++ src/assets/fa_validation.js
@@ -22,3 +22,24 @@
   });
 
 })(jQuery);
+
+(function($) {
+
+  $(document).on('submit', 'form#9', function(e) {
+    let invalid = 0;
+    $('form#9 .errMsg').remove();
+    if ($('#tfa_4 input:checked').length == 0) {
+      $('#tfa_4').parents('.oneField').append('<div class="errMsg"><span>This field is required.</span></div>');
+      invalid++;
+    }
+    if ($('#tfa_16 input:checked').length == 0) {
+      $('#tfa_16').parents('.oneField').append('<div class="errMsg"><span>This field is required.</span></div>');
+      invalid++;
+    }
+    if (invalid > 0) {
+      alert('The form is not complete and has not been submitted yet. There are ' + invalid + ' problems with your submission.');
+      return false;
+    }
+  });
+
+})(jQuery);
